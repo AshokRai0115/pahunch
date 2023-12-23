c439cd5d42e75f9360cc49c59b8d73d50f0c3917
AshokRai <bono.ashok@gmail.com>
2023-12-23 22:12:24 +0545

    navbar searchbar hide and show feature added

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const searchBar= document.getElementsByClassName('toggle-search');
+const searchToggler = document.getElementById('search-toggler');
+// searchBar.addEventListener('click', ()=>{
+//     searchBar
+// })
+const bar = document.getElementById('search-toggler');
+
+console.log(searchBar[0].classList);
+console.log(searchToggler);
+searchToggler.addEventListener('click', ()=>{
+    if(searchBar[0].classList.contains('hide')){
+        searchBar[0].classList.remove('hide');
+        searchBar[0].classList.add('show');
+        searchBar[0].classList.add('form-control');
+        
+    }else{
+        searchBar[0].classList.remove('show');
+        searchBar[0].classList.remove('form-control');
+        searchBar[0].classList.add('hide');
+    }
+    console.log(searchBar[0].classList)
+})
