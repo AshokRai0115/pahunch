0fe2e19c5287c96798b8cfa3be9cf43d8be7fd0f
AshokRai <bono.ashok@gmail.com>
2023-12-28 13:53:28 +0545

    toggle

--- assets/js/resources.js
+++ assets/js/resources.js
@@ -8,15 +8,18 @@ toggleCircle.addEventListener('click',()=>{
         toggleCircle.classList.remove("move-circle-left");
         toggleValue = "EN";
         toggleCircle.style.backgroundColor = "white";
+        toggleContainer.style.color = "white";
 
     }
     else{
         toggleContainer.classList.remove("toggle-background");
         toggleCircle.style.backgroundColor = " #0a66c2";
+        toggleContainer.style.color = "black";
         toggleCircle.classList.add("move-circle-left");
         toggleCircle.classList.remove("move-circle-right");
         toggleValue = "NE";
-
     }
     
 })
+
+new Splide( '.splide' ).mount();
